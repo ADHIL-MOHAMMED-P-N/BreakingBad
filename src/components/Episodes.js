2c5d10b17924e011e0b9f5b80f81ce829d86588e
Adhil-Mohammed-P-N <adhilmohammedpn@gmail.com>
2021-01-03 11:19:43 +0530

    episode filtering

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -3,16 +3,21 @@ import "../App.css";
 import "./Episodes.css";
 
 const Episodes = ({ episodes }) => {
-  //filtering season
+  //filtering season and episode
   const [season, setSeason] = useState("all");
+  const [episode, setEpisode] = useState("all");
+
   const seasonSearch = (val) => {
     setSeason(val);
+  };
+
+  const episodeSearch = (val) => {
+    setEpisode(val);
   };
   return (
     <>
       <h1 className="heading">Episodes</h1>
       <label for="season">Choose a Season : </label>
-
       <select
         name="Season"
         id="season"
@@ -25,14 +30,54 @@ const Episodes = ({ episodes }) => {
         <option value="2">Season 2</option>
         <option value="3">Season 3</option>
         <option value="4">Season 4</option>
+        <option value="5">Season 5</option>
+      </select>
+      {/* -----------------------episode search------------------ */}
+      <label for="episode">Choose a Episode: </label>
+      <select
+        name="episode"
+        id="episode"
+        onClick={(event) => {
+          episodeSearch(event.target.value);
+        }}
+      >
+        <option value="all">All</option>
+        <option value="1">Episode 1</option>
+        <option value="2">Episode 2</option>
+        <option value="3">Episode 3</option>
+        <option value="4">Episode 4</option>
+        <option value="5">Episode 5</option>
+        <option value="6">Episode 6</option>
+        <option value="7">Episode 7</option>
+        <option value="8">Episode 8</option>
+        <option value="9">Episode 9</option>
+        <option value="10">Episode 10</option>
+        <option value="11">Episode 11</option>
+        <option value="12">Episode 12</option>
+        <option value="13">Episode 13</option>
+        <option value="14">Episode 14</option>
+        <option value="15">Episode 15</option>
+        <option value="16">Episode 16</option>
       </select>
 
       <div className="episode__container">
         {episodes
           .filter((item) => {
+            if (item.series == "Breaking Bad") {
+              return item;
+            }
+          })
+          .filter((item) => {
             if (season == "all") {
               return item;
             } else if (season == item.season) {
+              return item;
+            }
+          })
+          .filter((item) => {
+            if (episode == "all") {
+              return item;
+            } else if (episode == item.episode) {
               return item;
             }
           })
